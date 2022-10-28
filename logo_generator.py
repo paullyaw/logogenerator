a3a55931fbf96ddfdb29ba1c5b62c13b320648b0
paullyaw <116737190+paullyaw@users.noreply.github.com>
2022-10-28 16:20:46 +0300

    Update logo_generator.py

--- logo_generator.py
+++ logo_generator.py
@@ -1,9 +1,9 @@
 import sys
-
 from PyQt5 import uic
 from PyQt5.QtCore import pyqtSignal
-from PyQt5.QtGui import QColor
-from PyQt5.QtWidgets import QApplication, QMainWindow, QColorDialog, QWidget
+import sqlite3
+from PyQt5.QtWidgets import QApplication, QMainWindow, QColorDialog, QWidget, QStackedWidget, QWizard
+from pyqt5_plugins.examplebutton import QtWidgets
 
 colorChanged = pyqtSignal()
 
@@ -12,13 +12,27 @@ class MainPage(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('logo1.ui', self)
+        self.colors = []
         self.btncolor.clicked.connect(self.choose_color)
+        self.btnnext.clicked.connect(self.second_step)
 
     def choose_color(self):
-        colors = []
         color_pallete = QColorDialog(self)
         if color_pallete.exec_():
-            colors.append(color_pallete.currentColor().name())
+            self.colors.append(color_pallete.currentColor().name())
+            print(self.colors)
+
+    def second_step(self):
+        if self.lineEdit.text() == "":
+            self.lineEdit.setPlaceholderText("*Обязательное поле для ввода")
+        else:
+            pass
+
+
+class SecondPage(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.uic.loadUi('logo2.ui', self)
 
 
 if __name__ == '__main__':
