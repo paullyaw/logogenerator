2a78a5ac7cec0f0fdb534487a6767131722a91dc
paullyaw <116737190+paullyaw@users.noreply.github.com>
2022-11-01 17:13:21 +0300

    Update logo_generator.py

--- logo_generator.py
+++ logo_generator.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5 import uic
 from PyQt5.QtCore import pyqtSignal
 import sqlite3
-from PyQt5.QtWidgets import QApplication, QMainWindow, QColorDialog, QWidget, QStackedWidget, QWizard
+from PyQt5.QtWidgets import QApplication, QMainWindow, QColorDialog, QWidget, QStackedWidget, QWizard, QMessageBox
 from pyqt5_plugins.examplebutton import QtWidgets
 
 colorChanged = pyqtSignal()
@@ -25,14 +25,38 @@ class MainPage(QMainWindow):
     def second_step(self):
         if self.lineEdit.text() == "":
             self.lineEdit.setPlaceholderText("*Обязательное поле для ввода")
+        elif not self.colors:
+            self.btncolor.setStyleSheet('QPushButton {color: red}')
+            self.btncolor.clicked.connect(self.change_btntext_color)
         else:
-            pass
+            self.window = SecondPage()
+            self.btnnext.clicked.connect(lambda: self.show_window(self.window))
+
+    def change_btntext_color(self):
+        self.btncolor.setStyleSheet('QPushButton {color: black}')
+
+    def show_window(self, window):
+        window.show()
 
 
 class SecondPage(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.uic.loadUi('logo2.ui', self)
+        uic.loadUi('logo2.ui', self)
+        self.setWindowTitle("Генератор Логотипов")
+
+        self.window = FavoritesPage()
+        self.btnfavorites.clicked.connect(lambda: self.show_window(self.window))
+
+    def show_window(self, window):
+        window.show()
+
+
+class FavoritesPage(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('logo3.ui', self)
+        self.setWindowTitle("Избранное")
 
 
 if __name__ == '__main__':
