eeb79b881fc5fd82e5697d4d3742b57896521971
paullyaw <116737190+paullyaw@users.noreply.github.com>
2022-11-03 17:40:32 +0300

    Update logo_generator.py

--- logo_generator.py
+++ logo_generator.py
@@ -1,9 +1,8 @@
 import sys
+import sqlite3
 from PyQt5 import uic
 from PyQt5.QtCore import pyqtSignal
-import sqlite3
-from PyQt5.QtWidgets import QApplication, QMainWindow, QColorDialog, QWidget, QStackedWidget, QWizard, QMessageBox
-from pyqt5_plugins.examplebutton import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QColorDialog
 
 colorChanged = pyqtSignal()
 
@@ -13,30 +12,40 @@ class MainPage(QMainWindow):
         super().__init__()
         uic.loadUi('logo1.ui', self)
         self.colors = []
-        self.btncolor.clicked.connect(self.choose_color)
-        self.btnnext.clicked.connect(self.second_step)
+        self.namebrand = ""
+        self.aboutbrand = ""
+        self.btncolor.clicked.connect(self.choose_color)  # Если пользователь нажал кнопку выбора цвета 
+        self.btnnext.clicked.connect(self.check_information)  # Если пользователь нажал кнопку "Дальше"
 
     def choose_color(self):
         color_pallete = QColorDialog(self)
         if color_pallete.exec_():
-            self.colors.append(color_pallete.currentColor().name())
-            print(self.colors)
+            self.colors.append(color_pallete.currentColor().name())  # Добавление основных цветов в БД
 
-    def second_step(self):
+    def check_information(self):
         if self.lineEdit.text() == "":
-            self.lineEdit.setPlaceholderText("*Обязательное поле для ввода")
+            self.lineEdit.setPlaceholderText("*Обязательное поле для ввода")  # Проверка ввел ли пользователь название
+            # бренда, для дальнейшей генерации 
         elif not self.colors:
+            self.btncolor.setStyleSheet('QPushButton {color: red}')  # Проверка выбрал ли пользователь цвета
+            self.btncolor.clicked.connect(self.change_btntext_color)
+        if self.lineEdit.text() == "" and not self.colors:  # Проверка выбрал ли пользователь цвета и ввел название
+            self.lineEdit.setPlaceholderText("*Обязательное поле для ввода")
             self.btncolor.setStyleSheet('QPushButton {color: red}')
             self.btncolor.clicked.connect(self.change_btntext_color)
-        else:
+        if self.lineEdit.text() != "" and self.colors:  # Добавление введенной информации в БД 
+            self.namebrand = self.lineEdit.text()
+            if self.textEdit.toPlainText() != "":
+                self.aboutbrand = self.textEdit.toPlainText()
             self.window = SecondPage()
             self.btnnext.clicked.connect(lambda: self.show_window(self.window))
 
     def change_btntext_color(self):
-        self.btncolor.setStyleSheet('QPushButton {color: black}')
+        self.btncolor.setStyleSheet('QPushButton {color: black}')  # Если пользователь выбрал цвета, изменение цвета 
+        # текста кнопки 
 
     def show_window(self, window):
-        window.show()
+        window.show()  # Показ слудующего окна 
 
 
 class SecondPage(QMainWindow):
@@ -49,7 +58,7 @@ class SecondPage(QMainWindow):
         self.btnfavorites.clicked.connect(lambda: self.show_window(self.window))
 
     def show_window(self, window):
-        window.show()
+        window.show()  # Показ слудующего окна 
 
 
 class FavoritesPage(QMainWindow):
@@ -64,3 +73,4 @@ if __name__ == '__main__':
     ex = MainPage()
     ex.show()
     sys.exit(app.exec_())
+
